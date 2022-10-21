7b63add0dc5a5c4bfb7ceaed8b86fdf367ab48aa
Normita Burce <pipermita@gmail.com>
2022-10-21 22:37:49 +0800

    --all

---
+++ js/kodego.js
@@ -0,0 +1,45 @@
+
+function getFlowerColor(flower){     
+    switch(flower){
+        case "rose":
+            return "red";
+        case "violet":
+            return "blue";
+        case "sunflower":
+            return "yellow";
+        default:
+            return "unknown";
+    }
+}
+
+function getCalendarMonthDays(monthName){
+    switch(monthName){
+        case "september":
+        case "april":
+        case "june":
+        case "november":
+            return 30;
+        case "february":
+            return 28;
+        default:
+            return 31;
+
+
+        
+    }
+
+}
+
+
+function CheckNumber() {
+    switch (operator) {
+      case "+":
+      return number1 + number2;
+      case "-":
+      return value1 - value2;
+      case "*":
+      return value1 * value2;
+      case "/":
+      return value1 / value2;
+    }
+}
